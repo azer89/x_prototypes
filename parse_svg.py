d7ad092b71dc77141cfeba6631c1e331648e9e39
azer89 <radhitya@uwaterloo.ca>
2015-12-28 15:51:43 -0500

    edited

--- parse_svg.py
+++ parse_svg.py
@@ -9,8 +9,31 @@ import pysvg.parser
 import cairo
 import svglib
 
-#from pysvg.structure import svg
+
 from pysvg.builders import ShapeBuilder
 
+from pysvg.structure import *
+from pysvg.core import *
+from pysvg.text import *
+
+"""
+# Example 1. load an svg then save it as another file
 svg_img = pysvg.parser.parse("ellipse.svg")
-print "size :", svg_img.get_width(), svg_img.get_height()
+svg_img.save("out_files/ellipse.svg")
+"""
+
+# Example 2. draw something
+svg_img = Svg(0, 0, width = "100%", height = "100%")
+txt = Text("HELLO WORLD", x = 0, y = 15)
+txt.set_stroke_width("1px")
+txt.set_stroke("#00C")
+txt.set_fill("none")
+txt.set_font_size("24")
+
+group = G()
+group.set_transform("rotate(-30)")
+group.addElement(txt)
+
+svg_img.addElement(group)
+svg_img.save("out_files/hello_world.svg")
+
