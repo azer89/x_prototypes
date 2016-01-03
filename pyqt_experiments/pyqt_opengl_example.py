965bb95c7a921ef0f404f6aafd8c3567f2ae7cb7
azer89 <radhitya@uwaterloo.ca>
2016-01-03 13:13:20 -0500

    restructuring

--- pyqt_experiments/pyqt_opengl_example.py
+++ pyqt_experiments/pyqt_opengl_example.py
@@ -1,6 +1,9 @@
 
 """
 https://github.com/Werkov/PyQt4/blob/master/examples/opengl/hellogl.py
+https://github.com/prideout/coregl-python
+http://stackoverflow.com/questions/11192938/compiling-shaders-in-pyqt
+
 """
 
 import sys
@@ -8,15 +11,16 @@ import math
 
 from PyQt4 import QtCore, QtGui, QtOpenGL
 from PyQt4.QtGui import QWidget
+from PyQt4.QtOpenGL import QGLShaderProgram, QGLShader, QGLFormat, QGLContext, QGLWidget
 
 try:
-    print "Do we have OpenGL ?"
     from OpenGL import GL
 except ImportError:
-    print "NO, you need to install PyOpenGL dude"
+    print "You need to install PyOpenGL"
     sys.exit(1)
 else:
-    print "YES !"
+    print "OpenGL OK"
+    
     
 class Window(QtGui.QWidget):
     """
@@ -39,7 +43,16 @@ class GLWidget(QtOpenGL.QGLWidget):
     """
     
     def __init__(self, parent=None):
-        super(GLWidget, self).__init__(parent)
+        #super(GLWidget, self).__init__(parent)
+        
+        if hasattr(QGLFormat, 'setVersion'):
+            f = QGLFormat(); f.setVersion(3, 3)
+            f.setProfile(QGLFormat.CoreProfile)
+            c = QGLContext(f, None)
+            QGLWidget.__init__(self, c, parent)
+            print "Version is set to 3.3"
+        else:
+            QGLWidget.__init__(self, parent)
         
         self.backColor = QtGui.QColor.fromRgbF(1.0, 0.0, 0.0, 1.0)
         
@@ -50,15 +63,37 @@ class GLWidget(QtOpenGL.QGLWidget):
         return QtCore.QSize(400, 400)
     
     def initializeGL(self):
+        
+        # shaders
+        self._shaderProgram = QGLShaderProgram()
+        if self._shaderProgram.addShaderFromSourceFile(QGLShader.Vertex, "shader.vert") :
+            print "Vertex shader OK"
+        if self._shaderProgram.addShaderFromSourceFile(QGLShader.Fragment, "shader.frag") :
+            print "Fragment shader OK"
+        print self._shaderProgram.log()
+        self._shaderProgram.bind() # or link()
+        
+        
+        self._mvpMatrixLocation = self._shaderProgram.uniformLocation("mvpMatrix")
+        self._colorLocation = self._shaderProgram.attributeLocation("vertexColor")
+        self._vertexLocation = self._shaderProgram.attributeLocation("vert")
+        self._use_color_location = self._shaderProgram.uniformLocation("use_color")
+                
+        
         self.qglClearColor(self.backColor)
-        #self.object = self.makeObject()
-        GL.glShadeModel(GL.GL_FLAT)
+        GL.glEnable(GL.GL_BLEND);
+        GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
         GL.glEnable(GL.GL_DEPTH_TEST)
-        GL.glEnable(GL.GL_CULL_FACE)
+        
+        #print self.width()
+        #print self.height()
     
     def paintGL(self):
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
         GL.glLoadIdentity()
+        
+    #def updateGL(self):
+    #    print "Update"
     
     
 if __name__ == '__main__':
