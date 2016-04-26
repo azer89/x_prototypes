46f6d9e40bc619c13b99cd627356784c04dc6991
Reza <radhitya@uwaterloo.ca>
2016-04-26 07:25:31 -0400

    edited

--- OpenCV_PCA/OpenCV_PCA/OpenCV_PCA.cpp
+++ OpenCV_PCA/OpenCV_PCA/OpenCV_PCA.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+#include "PathIO.h"
 #include "AVector.h"
 #include "AShape.h"
 #include "UtilityFunctions.h"
@@ -21,6 +22,8 @@ DATA
 */
 //std::vector<AShape> _shapes;
 std::vector<std::string> _fileStr;
+std::string _directoryPath = "D:\\Code\\x_prototypes\\OpenCV_PCA\\OpenCV_PCA\\Bunnies";
+float _line_thickness = 2.0f;
 
 /*
 ================================================================================
@@ -30,6 +33,7 @@ Function declarations
 void drawAxis(cv::Mat&, cv::Point, cv::Point, cv::Scalar, const float);
 double getOrientation(const std::vector<cv::Point> &, cv::Mat&);
 void LoadShapes();
+void DrawPolygon(cv::Mat& drawing, std::vector<AVector> shape_contours, cv::Scalar color, float xOffset = 0, float yOffset = 0);
 
 /*
 ================================================================================
@@ -50,8 +54,8 @@ void LoadShapes()
 	struct dirent *ep;
 
 	//std::vector<std::string> fileStr;
-	std::string directoryPath = "D:\\Code\\GeometricMoments\\Bunnies";
-	dp = opendir(directoryPath.c_str());
+	//std::string directoryPath = "D:\\Code\\x_prototypes\\OpenCV_PCA\\OpenCV_PCA\\Bunnies";
+	dp = opendir(_directoryPath.c_str());
 	if (dp != NULL)
 	{
 		while (ep = readdir(dp)) { _fileStr.push_back(ep->d_name); }
@@ -59,12 +63,12 @@ void LoadShapes()
 	}
 	else { perror("Couldn't open the directory"); }
 
-	for (int a = 0; a < _fileStr.size(); a++)
+	/*for (int a = 0; a < _fileStr.size(); a++)
 	{
 		if (_fileStr[a] == "." || _fileStr[a] == "..") { continue; }
 		std::string fileName = directoryPath + "\\" + _fileStr[a];
 		std::cout << "filepath: " << fileName << "\n";
-	}
+	}*/
 }
 
 /*
@@ -102,7 +106,8 @@ void drawAxis(cv::Mat& img, cv::Point p, cv::Point q, cv::Scalar colour, const f
 Function getOrientation
 ================================================================================
 */
-double getOrientation(const std::vector<cv::Point> &pts, cv::Mat &img)
+//double getOrientation(const std::vector<cv::Point> &pts, cv::Mat &img)
+double getOrientation(const std::vector<AVector> &pts, cv::Mat &img)
 {
 	//Construct a buffer used by the pca analysis
 	int sz = static_cast<int>(pts.size());
@@ -135,9 +140,44 @@ double getOrientation(const std::vector<cv::Point> &pts, cv::Mat &img)
 	drawAxis(img, cntr, p1, cv::Scalar(0, 255, 0), 1);
 	drawAxis(img, cntr, p2, cv::Scalar(255, 255, 0), 5);
 
+	cv::Point p3 = cntr - 0.02 * cv::Point(eigen_vecs[0].x * eigen_val[0], eigen_vecs[0].y * eigen_val[0]);
+	cv::Point p4 = cntr + 0.02 * cv::Point(eigen_vecs[1].x * eigen_val[1], eigen_vecs[1].y * eigen_val[1]);
+	drawAxis(img, cntr, p3, cv::Scalar(0, 255, 0), 1);
+	drawAxis(img, cntr, p4, cv::Scalar(255, 255, 0), 5);
+
 	double angle = atan2(eigen_vecs[0].y, eigen_vecs[0].x); // orientation in radians
 
 	return angle;
+}
+
+/*
+================================================================================
+Drawing function
+================================================================================
+*/
+void DrawPolygon(cv::Mat& drawing, std::vector<AVector> shape_contours, cv::Scalar color, float xOffset, float yOffset)
+{
+	std::vector<cv::Point2f> new_contours;
+	cv::RNG rng(12345);
+	for (size_t b = 0; b < shape_contours.size() - 1; b++)
+		//for (size_t b = 0; b < 990; b++)
+	{
+
+		//float randomX = ((float)rng.uniform(-500, 500)) / 500.0f;
+		//float randomY = ((float)rng.uniform(-500, 500)) / 500.0f;
+		//std::cout << randomX << " " << randomY << "\n";
+		//new_contours.push_back(cv::Point2f((shape_contours[b].x + xOffset + randomX),
+		//	                               (shape_contours[b].y + yOffset + randomY))); 
+		new_contours.push_back(cv::Point2f((shape_contours[b].x + xOffset),
+			(shape_contours[b].y + yOffset)));
+	}
+
+
+	for (size_t b = 0; b < new_contours.size() - 1; b++)
+	{
+		cv::line(drawing, new_contours[b], new_contours[b + 1], color, _line_thickness);
+	}
+	cv::line(drawing, new_contours[new_contours.size() - 1], new_contours[0], color, _line_thickness);
 }
 
 
@@ -147,6 +187,38 @@ double getOrientation(const std::vector<cv::Point> &pts, cv::Mat &img)
 */
 int _tmain(int argc, _TCHAR* argv[])
 {
+	PathIO* pathIO = new PathIO();
+	//cv::Mat drawing = cv::Mat::zeros(cv::Size(1000, 1000), CV_8UC3);
+	std::vector<cv::Mat> drawings;
+
+	LoadShapes();
+
+	cv::RNG rng(12345);
+	for (int a = 0; a < _fileStr.size(); a++)
+	{
+		if (_fileStr[a] == "." || _fileStr[a] == "..") { continue; }
+		std::string fileName = _directoryPath + "\\" + _fileStr[a];
+		std::cout << "filepath: " << fileName << "\n";
+
+		std::vector<AVector> ori_contour;
+		std::vector<AShape> shapes = pathIO->LoadPath(fileName);
+		ori_contour = shapes[0].shape_contours;
+		std::vector<AVector> shape_contour;
+		UtilityFunctions::UniformResample(ori_contour, shape_contour, 1000);
+
+
+		//if (a == 4)
+		//{
+		cv::Mat drawing = cv::Mat::zeros(cv::Size(1000, 1000), CV_8UC3);
+		getOrientation(shape_contour, drawing);
+
+		cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+		DrawPolygon(drawing, shape_contour, color);
+
+		drawings.push_back(drawing);
+		//}
+	}
+
 	/*
 	// Load image
 	cv::Mat src = cv::imread("pca_test4.jpg");
@@ -190,7 +262,21 @@ int _tmain(int argc, _TCHAR* argv[])
 	cv::imshow("output", src);
 	*/
 
+	for (size_t a = 0; a < drawings.size(); a++)
+	{
+		std::string title = "drawing " + std::to_string(a);
+		//cv::namedWindow(title.c_str(), CV_WINDOW_NORMAL); // CV_WINDOW_AUTOSIZE
+		cv::namedWindow(title.c_str(), CV_WINDOW_AUTOSIZE);
+		cv::imshow(title.c_str(), drawings[a]);
+	}
+
+	//cv::namedWindow("DRAWING", CV_WINDOW_NORMAL);
+	//cv::imshow("DRAWING", drawing);
+
 	cv::waitKey(0);
+
+	delete pathIO;
+
 	return 0;
 }
 
