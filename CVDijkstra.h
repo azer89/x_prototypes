acca419d27cb8814406480507a326053a8930f05
Reza <radhitya@uwaterloo.ca>
2016-11-28 12:25:59 -0500

    Create CVDijkstra.h

---
+++ CVDijkstra.h
@@ -0,0 +1,14 @@
+
+// http_://codereview.stackexchange.com/questions/66724/more-optimized-approach-of-dijkstras-algorithm
+
+#ifndef ___CV_DIJKSTRA___
+#define ___CV_DIJKSTRA___
+
+class CVDijkstra
+{
+public:
+	static void DijkstraTest();
+
+};
+
+#endif
